809c218205990ac725ff795f57295432817e1040
Tuetuopay <tuetuopay@me.com>
2017-03-06 15:34:53 +0100

    Added switches to disable IPv4/6 record updates

--- update.py
+++ update.py
@@ -59,6 +59,12 @@ def main():
     parser.add_option("-s", "--subdomain",
                       help="subdomain to act on, empty for root",
                       action="store", dest="subdomain", default="")
+    parser.add_option("-4", "--no-ipv4",
+                      help="disable IPv4 record update",
+                      action="store_true", default=False, dest="noipv4")
+    parser.add_option("-6", "--no-ipv6",
+                      help="disable IPv6 record update",
+                      action="store_true", default=False, dest="noipv6")
     parser.usage = """%prog [options]"""
     parser.description = "Updates the A and AAAA (if available) records for" + \
         " subdomain.domain with the IPs from interface."
@@ -79,18 +85,21 @@ def main():
 
     # Fetch IP addresses
     addr = ni.ifaddresses(interface)
-    try:
-        ipv4 = addr[ni.AF_INET][0]['addr']
-        print("Setting A record to %s" % (ipv4))
-        print(setRecord(client, domain, subdomain, 'A', ipv4))
-    except e:
-        print("No IPv4 found for interface %s" % (interface))
-    try:
-        ipv6 = addr[ni.AF_INET6][0]['addr']
-        print("Setting AAAA record to %s" % (ipv6))
-        print(setRecord(client, domain, subdomain, 'AAAA', ipv6))
-    except e:
-        print("No IPv6 found for interface %s" % (interface))
+    if not options.noipv4:
+        try:
+            ipv4 = addr[ni.AF_INET][0]['addr']
+            print("Setting A record to %s" % (ipv4))
+            print(setRecord(client, domain, subdomain, 'A', ipv4))
+        except e:
+            print("No IPv4 found for interface %s" % (interface))
+
+    if not options.noipv6:
+        try:
+            ipv6 = addr[ni.AF_INET6][0]['addr']
+            print("Setting AAAA record to %s" % (ipv6))
+            print(setRecord(client, domain, subdomain, 'AAAA', ipv6))
+        except e:
+            print("No IPv6 found for interface %s" % (interface))
 
 if __name__ == '__main__':
     main()
